a386f6cdd3553c19793038638e45651155d431a6
yoannlg <shinji51@hotmail.fr>
2018-01-16 23:39:21 +0100

    Initialisation Projet

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+
+function init() {
+	var canvas = document.createElement('canvas');
+	canvas.width = 900;
+	canvas.height = 600;
+	canvas.style.border = "1px solid";
+
+
+	document.body.appendChild(canvas);
+
+}
+}
