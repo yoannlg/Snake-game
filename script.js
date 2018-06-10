4fe8ceff75bcf9c99a11c73659b24d316b539507
yoannlg <shinji51@hotmail.fr>
2018-06-10 18:02:48 +0200

    Snake Game :Finish: Adding style

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ window.onload = function () {
 		var canvas = document.createElement('canvas');
 		canvas.width = canvasWidth;
 		canvas.height = canvasHeight;
-		canvas.style.border = "1px solid";
-		canvas.style.display = 'flex';
-		canvas.style.margin = 'auto';
+		canvas.style.border = "30px solid grey";
+		canvas.style.display = 'block';
+		canvas.style.margin = '50px auto';
+		canvas.style.backgroundColor = '#ddd';
 		document.body.appendChild(canvas);
 		ctx = canvas.getContext('2d');
 		snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
@@ -44,17 +45,29 @@ window.onload = function () {
 				
 			}
 			ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+			drawScore();
 			snakee.draw();
 			applee.draw();
-			drawScore();
 			setTimeout(refreshCanvas, delay);
 		}
 	}
 	function gameOver()
 	{
 		ctx.save();
-		ctx.fillText("Game Over", 5, 15);
-		ctx.fillText("Appuyer sur la touche Espace pour rejouer",5, 30);
+		ctx.font = 'bold 70px sans-serif';
+		ctx.fillStyle = '#000';
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.strokeStyle = 'white';
+		ctx.lineWidth = 5;
+		var centerX = canvasWidth / 2;
+		var centerY = canvasHeight / 2;
+		ctx.strokeText("Game Over", centerX, centerY - 180);
+		ctx.fillText("Game Over", centerX, centerY - 180);
+		ctx.font = 'bold 30px sans-serif';
+
+		ctx.strokeText("Appuyer sur la touche Espace pour rejouer", centerX, centerY - 120);
+		ctx.fillText("Appuyer sur la touche Espace pour rejouer",centerX, centerY - 120);
 		ctx.restore();
 	}
 	function restart()
@@ -67,7 +80,13 @@ window.onload = function () {
 	function drawScore()
 	{
 		ctx.save();
-		ctx.fillText(score.toString(), 5, canvasHeight - 5);
+		ctx.font = 'bold 200px sans-serif';
+		ctx.fillStyle = 'gray';
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		var centerX = canvasWidth / 2;
+		var centerY = canvasHeight / 2;
+		ctx.fillText(score.toString(), centerX, centerY);
 		ctx.restore();
 
 	}
